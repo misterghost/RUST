9008afac41b949642c48a47f565a65d3095e5b38
misterghost <arquitectolizcano@gmail.com>
2018-02-12 16:10:54 -0600

    finished binding and patterns

--- binding_and_patterns/src/main.rs
+++ binding_and_patterns/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // binding:
     // x is a binding of type 32 bits, and the number 5
     let x :i32 = 5;
     // using patterns to binding multiple values
@@ -8,7 +9,8 @@ fn main() {
     // print result using interpolation
     // text, curly, variable to go inside curly
     println!("The result is:  {}", result);
-    // mutability
+
+    // mutability:
     // this will give an error, because we are changing 
     // the value of y without stating it beforehand
     // let y = 5;
@@ -22,6 +24,7 @@ fn main() {
     // still gives a warning since we stated
     // that it was mutable but we havent mutated it
 
+    // initialization:
     // cant use a variable that has not been initiated
     // this will give an error since you have specified
     // that e will be an integer of 16 bits, but its 
@@ -31,8 +34,36 @@ fn main() {
     // let e: i16 = 245;
     println!("The value of e is: {}", e);
 
+    // scope:
+    // we are inside the MAIN's scope
+    let h: i32 = 17;
+    {
+        let g: i32 = 3;
+        println!("value of h is {} and value of g is {}", g, h );
+        // this works because we are in the inside scope
+        // and we have access to both G and H
+    }
+    println!("value of h is {} and value of g is {}", g, h );
+    // this wont work since we are outside of the scope for g:
+    // error-> "Cannot find value of G in this scope"
 
+    // variable bindings can be SHADOWED:
+    let s: i32 = 8;
+    {
+        println!("{}", s);      // this prints "8"
+        let s = 12;
+        println!("{}", s);      // this prints "12"
+    }
+    println!("{}", s);          // this prints "8"
+    let s = 42;
+    println!("{}", s);          // this prints "42"
 
+    // shadowing can alter the type 
+    let mut w: i32 = 23;        // w is a mutable integer
+    // then we change w
+    w = 76;                     // now w is immutable, & bound to 76
+    let m = 4;                  // m is an integer
+    let m = "i can be text";    // now m is of different type
 
 
 
